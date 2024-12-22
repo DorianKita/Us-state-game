1006afe306e04f40e479279aac45e3f3e16befa2
Dorian Kita <dorian.kita90@gmail.com>
2024-12-22 19:01:23 +0100

    added creation of states to learn csv file after exit command, so that can be educational game.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ correct_guessess = []
 while len(correct_guessess) != 50:
     answer_state = screen.textinput(title=f"{len(correct_guessess)}/50 States Correct",
                                     prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
     if answer_state in states:
         correct_guessess.append(answer_state)
         new_turtle = turtle.Turtle()
@@ -23,4 +26,15 @@ while len(correct_guessess) != 50:
         new_turtle.goto(x=int(data[data.state == answer_state].x), y=int(data[data.state == answer_state].y))
         new_turtle.write(answer_state, font=('Arial', 10, 'bold'))
 
-screen.exitonclick()
+
+to_learn = []
+for state in states:
+    if state not in correct_guessess:
+        to_learn.append(state)
+
+states_to_learn = {
+    "States to learn" : to_learn
+}
+
+df = pandas.DataFrame(states_to_learn)
+df.to_csv('States to learn.csv')
